5bd80679f09a04a09fa21cf6b658372fe2e75bc4
oohira <oohira.dev@gmail.com>
2018-01-06 09:52:28 +0900

    Add a SegmentedControl, Switches, a stretch image Button

--- ControlFun/ControlFun/ViewController.swift
+++ ControlFun/ControlFun/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var numberField: UITextField!
     @IBOutlet weak var sliderLabel: UILabel!
+    @IBOutlet weak var leftSwitch: UISwitch!
+    @IBOutlet weak var rightSwitch: UISwitch!
+    @IBOutlet weak var doSomethingButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,5 +33,59 @@ class ViewController: UIViewController {
 
     @IBAction func onSliderChanged(_ sender: UISlider) {
         sliderLabel.text = "\(lroundf(sender.value))"
+    }
+
+    @IBAction func toggleControls(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            // "Switches" is selected
+            leftSwitch.isHidden = false
+            rightSwitch.isHidden = false
+            doSomethingButton.isHidden = true
+        } else {
+            leftSwitch.isHidden = true
+            rightSwitch.isHidden = true
+            doSomethingButton.isHidden = false
+        }
+    }
+
+    @IBAction func onSwitchChanged(_ sender: UISwitch) {
+        let setting = sender.isOn
+        leftSwitch.setOn(setting, animated: true)
+        rightSwitch.setOn(setting, animated: true)
+    }
+
+    @IBAction func onButtonPressed(_ sender: UIButton) {
+        let controller = UIAlertController(title: "Are You Sure?",
+                                           message: nil,
+                                           preferredStyle: .actionSheet)
+        let yesHandler : (UIAlertAction) -> Void = { action in
+            var msg = "You can breathe easy, everything went OK."
+            if let name = self.nameField.text {
+                msg += " \(name)"
+            }
+            let controller2 = UIAlertController(title:"Something Was Done",
+                                                message: msg,
+                                                preferredStyle: .alert)
+            let cancelAction = UIAlertAction(title: "Phew!",
+                                             style: .cancel,
+                                             handler: nil)
+            controller2.addAction(cancelAction)
+            self.present(controller2, animated: true, completion: nil)
+        }
+        let yesAction = UIAlertAction(title: "Yes, I'm sure!",
+                                      style: .destructive,
+                                      handler: yesHandler)
+        let noAction = UIAlertAction(title: "No way!",
+                                     style: .cancel,
+                                     handler: nil)
+        controller.addAction(yesAction)
+        controller.addAction(noAction)
+        if let ppc = controller.popoverPresentationController {
+            // for iPad
+            ppc.sourceView = sender
+            ppc.sourceRect = sender.bounds
+            ppc.permittedArrowDirections = .down
+        }
+        present(controller, animated: true, completion: nil)
     }
 }
