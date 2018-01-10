089cb1b9a8c0b6478274f395959e60884422fb56
oohira <oohira.dev@gmail.com>
2018-01-10 23:16:02 +0900

    Add multi-component Picker

--- Pickers/Pickers/DoubleComponentPickerViewController.swift
+++ Pickers/Pickers/DoubleComponentPickerViewController.swift
@@ -8,7 +8,20 @@
 
 import UIKit
 
-class DoubleComponentPickerViewController: UIViewController {
+class DoubleComponentPickerViewController:
+    UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+    @IBOutlet weak var doublePicker: UIPickerView!
+
+    private let fillingComponent = 0
+    private let breadComponent = 1
+    private let fillingTypes = [
+        "Ham", "Turkey", "Peanut Butter", "Tuna Salad",
+        "Chicken Salad", "Roast Beef", "Vegemite"
+    ]
+    private let breadTypes = [
+        "White", "Whole Wheat", "Rye", "Sourdough", "Seven Grain"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +34,40 @@ class DoubleComponentPickerViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2;
     }
-    */
 
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == breadComponent {
+            return breadTypes.count
+        } else {
+            return fillingTypes.count
+        }
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == breadComponent {
+            return breadTypes[row]
+        } else {
+            return fillingTypes[row]
+        }
+    }
+
+    @IBAction func onButtonPressed(_ sender: UIButton) {
+        let fillingRow = doublePicker.selectedRow(inComponent: fillingComponent)
+        let breadRow = doublePicker.selectedRow(inComponent: breadComponent)
+
+        let filling = fillingTypes[fillingRow]
+        let bread = breadTypes[breadRow]
+
+        let message = "Your \(filling) on \(bread) bread will be right up."
+        let alert = UIAlertController(
+            title: "Thank you for your order",
+            message: message,
+            preferredStyle: .alert)
+        let action = UIAlertAction(title: "Great", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
