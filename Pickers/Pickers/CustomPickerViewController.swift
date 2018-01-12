4224ec539876a5daba8c3c493b012b08a5027b9b
oohira <oohira.dev@gmail.com>
2018-01-12 22:12:37 +0900

    Add a custom Picker

--- Pickers/Pickers/CustomPickerViewController.swift
+++ Pickers/Pickers/CustomPickerViewController.swift
@@ -8,12 +8,28 @@
 
 import UIKit
 
-class CustomPickerViewController: UIViewController {
+class CustomPickerViewController:
+UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var winLabel: UILabel!
+
+    private var images: [UIImage]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        images = [
+            UIImage(named: "seven")!,
+            UIImage(named: "bar")!,
+            UIImage(named: "crown")!,
+            UIImage(named: "cherry")!,
+            UIImage(named: "lemon")!,
+            UIImage(named: "apple")!
+        ]
+        winLabel.text = " " // Note the space between the quotes
+        arc4random_stir()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +37,39 @@ class CustomPickerViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
     }
-    */
 
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return images.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let image = images[row]
+        let imageView = UIImageView(image: image)
+        return imageView
+    }
+
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 64
+    }
+
+    @IBAction func spin(_ sender: UIButton) {
+        var numInRow = 1
+        var lastVal = -1
+        for i in 0..<3 {
+            let newValue = Int(arc4random_uniform(UInt32(images.count)))
+            if newValue == lastVal {
+                numInRow += 1
+            }
+            lastVal = newValue
+            picker.selectRow(newValue, inComponent: i, animated: true)
+            picker.reloadComponent(i)
+        }
+        let win = (numInRow == 3)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.winLabel.text = win ? "WINNER!" : " "
+        }
+    }
 }
